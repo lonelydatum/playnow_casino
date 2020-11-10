b1df7b186a2f2c3aedb804fc17e97c5ff753a3d2
Gar <gar@lonelydatu.com>
2020-11-09 22:29:51 -0500

    d

--- dev/fortune-coin---v1_300x250/_bundled/log-free.js
+++ dev/fortune-coin---v1_300x250/_bundled/log-free.js
@@ -112,13 +112,14 @@ function start() {
 	tl.from(".t1", .4, { opacity: 0 }, "+=.3");
 
 	tl.add("t1-out", "+=2.7");
-	tl.to(".t1", speed, { opacity: 0, x: "+=300" }, "t1-out");
-	tl.to(".dragon", speed, { opacity: 0, x: "+=300" }, "t1-out");
+	tl.to(".t1", speed, { opacity: 0, y: "+=300" }, "t1-out");
+	tl.to(".dragon", speed, { opacity: 0, y: "+=300" }, "t1-out");
 
 	tl.add("t2-in");
-	tl.from(".t2", speed, { x: "-=300" }, "t1-out");
-	tl.from(".screen", speed, { x: "-=300" }, "t1-out");
-	tl.from(".dragon2", speed, { x: "-=90" }, "t1-out+=.3");
+
+	tl.from(".screen", speed, { y: "-=300" }, "t1-out");
+	tl.from(".dragon2", speed, { y: "-=300" }, "t1-out+=.3");
+	tl.from(".t2", .4, { y: "-=80" });
 
 	tl.add("end", "+=3");
 
