0f93a2eb4e911344d66f4bd343a86bff11035cb3
Gar <gar@lonelydatu.com>
2020-11-10 10:18:34 -0500

    d

--- dev/live---v1_300x250/_js/main.js
+++ dev/live---v1_300x250/_js/main.js
@@ -1,35 +1,9 @@
 import {size, init, slicerTween} from '../../_common/js/common.js'
+import {live_1} from '../../_common/js/live.js'
 
 
 
-function start(){
-	
-	const tl = init()
-	
-	
-
-
-	tl.from(".t1a", .3, {opacity:0}, "+=.3")
-	tl.from(".t1b", .3, {opacity:0}, "+=.5")
-	
-
-	tl.from(".gold", .3, {x:size.w, ease:Power4.easeOut}, "+=2")
-	tl.from(".bg2", .3, {x:size.w, ease:Power4.easeOut}, "+=.1")
-	tl.to(".bg2", .4, {scale:.5, ease:Back.easeOut}, "+=.1")
-	
-
-	
-	
-
-	
-
-	tl.add(slicerTween(), "+=4")
-
-	
-		
-}
-
-start()
+live_1()
 
 
 module.exports = {};
