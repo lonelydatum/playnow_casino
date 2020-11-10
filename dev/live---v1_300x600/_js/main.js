b1dfaf6fa28151ce92bba68c994d2368834dc931
Gar <gar@lonelydatu.com>
2020-11-10 13:02:55 -0500

    d

---
+++ dev/live---v1_300x600/_js/main.js
@@ -0,0 +1,13 @@
+import {size, init, slicerTween} from '../../_common/js/common.js'
+
+import {live_1} from '../../_common/js/live.js'
+
+
+
+
+
+live_1()
+
+
+module.exports = {};
+
---
+++ dev/live---v2_300x250/_js/main.js
@@ -0,0 +1,10 @@
+import {size, init, slicerTween} from '../../_common/js/common.js'
+import {live_2} from '../../_common/js/live.js'
+
+
+
+live_2()
+
+
+module.exports = {};
+
